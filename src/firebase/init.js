472162bd2a0f9681a24db058f2155929639bf8a3
schemak@pulipinc.com <schemak@pulipinc.com>
2021-01-05 09:27:35 +0900

    .

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -4,9 +4,9 @@ import 'firebase/auth'
 // The configuration below is not sensitive data. You can serenely add your config here
 const config = {
   apiKey: 'AIzaSyCe6CSzOUs6_Ij_VHa6hancdEi6CNBtw7c',
-  authDomain: 'bento-starter.firebaseapp.com',
+  authDomain: 'salary-korea.web.app/',
   databaseURL: 'https://bento-starter.firebaseio.com',
-  projectId: 'bento-starter',
+  projectId: 'salary-korea',
   storageBucket: 'bento-starter.appspot.com',
   messagingSenderId: '458464977217'
 }
