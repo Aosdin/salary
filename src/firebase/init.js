e2e6bc7c45a88731aae5467f41657c37c77a4ef1
schemak@pulipinc.com <schemak@pulipinc.com>
2021-01-18 16:29:37 +0900

    추가

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -4,7 +4,7 @@ import 'firebase/auth'
 // The configuration below is not sensitive data. You can serenely add your config here
 const config = {
   apiKey: 'AIzaSyAlqvqm8G1r-9GhT-21ilYGYTJG0uI50aA',
-  authDomain: '1million-dollar.com/',
+  authDomain: '1million-dollar.com',
   databaseURL: '',
   projectId: 'salary-korea',
   storageBucket: 'bento-starter.appspot.com',
